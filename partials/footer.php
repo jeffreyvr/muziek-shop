49dd3b5fab376159eeeb72118522dc56d9111eed
Jeffrey van Rossum <j.vanrossum@eisma.nl>
2018-06-12 21:25:10 +0200

    Footer styling and product clickable.

--- partials/footer.php
+++ partials/footer.php
@@ -2,7 +2,7 @@
 <!-- Eind Main -->
 
 <!-- Start Footer -->
-<footer>Footer &copy; <?php echo date('Y'); ?></footer>
+<footer><?php echo SITE_NAME; ?> &copy; <?php echo date('Y'); ?></footer>
 <!-- Eind footer -->
 
 </body>
--- partials/producten.php
+++ partials/producten.php
@@ -5,9 +5,8 @@
 
     <!-- Start Product <?php echo $product['PRODUCTNUMMER']; ?> -->
     <article class="col product-item">
-      <a href="index.php?page=product&amp;productnummer=<?php echo $product['PRODUCTNUMMER']; ?>">
-        <img src="<?php echo $product['AFBEELDING_KLEIN']; ?>" alt="<?php echo $product['PRODUCTNAAM']; ?>">
-      </a>
+      <a href="index.php?page=product&amp;productnummer=<?php echo $product['PRODUCTNUMMER']; ?>"></a>
+      <img src="<?php echo $product['AFBEELDING_KLEIN']; ?>" alt="<?php echo $product['PRODUCTNAAM']; ?>">
       <h3><?php echo $product['PRODUCTNAAM']; ?></h3>
       <strong class="price"><?php echo han_format_price( $product['PRIJS'] ); ?></strong>
     </article>
